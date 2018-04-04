012c556e6f0f83c9ec827aae7639d5ccc30841d6
Diego Rueda <diego.rueda2@gmail.com>
2018-04-03 22:36:48 -0400

    Definición de permisos de mi sistema ()seeder

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -11,6 +11,92 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Navegar usuarios',
+            'slug' => 'users.index',
+            'description' => 'Lista u=y navega todos los usuarios del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Ver detalles de usuarios',
+            'slug' => 'users.show',
+            'description' => 'Ver en detalle cada usuarios del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Edicion de  usuarios',
+            'slug' => 'users.edit',
+            'description' => 'Editar los usuarios del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'eliminar usuarios',
+            'slug' => 'users.destroy',
+            'description' => 'Eliminar cualquier  usuarios del sistema'
+        ]);
+
+        //roles
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Navegar roles',
+            'slug' => 'roles.index',
+            'description' => 'Lista u=y navega todos los rol del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Ver detalles de roles',
+            'slug' => 'roles.show',
+            'description' => 'Ver en detalle cada rol del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Creacion de roles',
+            'slug' => 'roles.create',
+            'description' => 'Ver en detalle cada rol del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Edicion de  roles',
+            'slug' => 'roles.edit',
+            'description' => 'Editar los rol del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'eliminar roles',
+            'slug' => 'roles.destroy',
+            'description' => 'Eliminar cualquier  rol del sistema'
+        ]);
+
+        //productos
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Navegar productos',
+            'slug' => 'products.index',
+            'description' => 'Lista u=y navega todos los productos del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Ver detalles de productos',
+            'slug' => 'products.show',
+            'description' => 'Ver en detalle cada producto del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Creacion de productos',
+            'slug' => 'products.create',
+            'description' => 'Ver en detalle cada producto del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'Edicion de  productos',
+            'slug' => 'products.edit',
+            'description' => 'Editar los producto del sistema'
+        ]);
+
+        \Caffeinated\Shinobi\Models\Permission::create([
+            'name' => 'eliminar productos',
+            'slug' => 'products.destroy',
+            'description' => 'Eliminar cualquier  producto del sistema'
+        ]);
     }
 }
