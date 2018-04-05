24b754699fdb99e08bc70ebda5b07394d391d5b1
Diego Rueda <diego.rueda2@gmail.com>
2018-04-04 20:19:11 -0400

    sedd para roles y permisoss

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Caffeinated\Shinobi\Models;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(\App\User::class, 20)->create();
+
+        Models\Role::create([
+            'name' => 'Admin',
+            'slug' => 'admin',
+            'special' => 'all-access'
+        ]);
     }
 }
