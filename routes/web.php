5a5cc1e32bb39f085209039f1633bd457f2ca90e
Diego Rueda <diego.rueda2@gmail.com>
2018-04-18 22:23:12 -0400

    8 - Rutas y asignación de permisos | Rimorsoft Online

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,30 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 //Routes
 
+Route::middleware(['auth'])->group(function (){
+    //Roles
+    Route::post('roles/store', 'RoleController@store')->nmae('roles.store')->middleware('permission:roles.create');
+    Route::get('roles', 'RoleController@index')->nmae('roles.index')->middleware('permission:roles.index');
+    Route::get('roles/create', 'RoleController@create')->nmae('roles.create')->middleware('permission:roles.create');
+    Route::put('roles/{role}', 'RoleController@update')->nmae('roles.update')->middleware('permission:roles.edit');
+    Route::get('roles/{role}', 'RoleController@show')->nmae('roles.show')->middleware('permission:roles.show');
+    Route::delete('roles/{role}', 'RoleController@destroy')->nmae('roles.destroy')->middleware('permission:roles.destroy');
+    Route::get('roles/{role}', 'RoleController@edit')->nmae('roles.edit')->middleware('permission:roles.edit');
+
+    //Product
+    Route::post('products/store', 'ProductController@store')->nmae('products.store')->middleware('permission:products.create');
+    Route::get('products', 'ProductController@index')->nmae('products.index')->middleware('permission:products.index');
+    Route::get('products/create', 'ProductController@create')->nmae('products.create')->middleware('permission:products.create');
+    Route::put('products/{role}', 'ProductController@update')->nmae('products.update')->middleware('permission:products.edit');
+    Route::get('products/{role}', 'ProductController@show')->nmae('products.show')->middleware('permission:products.show');
+    Route::delete('products/{role}', 'ProductController@destroy')->nmae('products.destroy')->middleware('permission:products.destroy');
+    Route::get('products/{role}', 'ProductController@edit')->nmae('products.edit')->middleware('permission:products.edit');
+
+    //User
+    Route::get('users', 'UserController@index')->nmae('users.index')->middleware('permission:users.index');
+    Route::put('users/{role}', 'UserController@update')->nmae('users.update')->middleware('permission:users.edit');
+    Route::get('users/{role}', 'UserController@show')->nmae('users.show')->middleware('permission:users.show');
+    Route::delete('users/{role}', 'UserController@destroy')->nmae('users.destroy')->middleware('permission:users.destroy');
+    Route::get('users/{role}', 'UserController@edit')->nmae('users.edit')->middleware('permission:users.edit');
+
+});
